352d036c2d7c8e52f24db33a9ff304749c5e7ed0
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-10-31 10:22:46 +0530

    Create verilog.v

---
+++ JK_flipflop/verilog.v
@@ -0,0 +1,16 @@
+
+module J_K_FF(
+input J,K,clk,
+output reg Q,
+output Qbar);
+always@(posedge clk)
+begin 
+case ({J,K})
+2'b00 : Q <= Q;
+2'b01 : Q <= 1'b0;
+2'b10 : Q <= 1'b1;
+2'b11 : Q <= ~Q;
+endcase
+end
+assign Qbar = ~Q;
+endmodule
