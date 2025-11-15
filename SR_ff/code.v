2e7c08346afd2871fcc6358a066b5f08cf4a4667
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-11-15 08:33:57 +0530

    Create code.v

---
+++ SR_ff/code.v
@@ -0,0 +1,17 @@
+
+module SR_FLIP_FLOP(
+input S,R,clk,
+output reg Q,
+output Qbar);
+
+always@( posedge clk )
+begin
+case ({S,R})
+2'b00 : Q <= Q;
+2'b01 : Q <= 1'b0;
+2'b10 : Q <= 1'b1;
+2'b11 : Q <= 1'bx;
+ endcase
+end
+assign Qbar = ~Q;
+endmodule
